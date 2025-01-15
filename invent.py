687d5bada961d1994659ea2725018e4e63ec018f
Kaushal <157907997+KAUSHAL36977@users.noreply.github.com>
2025-01-15 18:29:38 +0530

    Update invent.py

--- invent.py
+++ invent.py
@@ -1,1 +1,29 @@
+# settings.py
+INSTALLED_APPS = [
+    ...
+    'rest_framework',
+    'api',
+]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',  # Default JSON responses
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'inventory',
+        'ENFORCE_SCHEMA': True,
+        'CLIENT': {
+            'host': 'your_mongo_host',  # Add your MongoDB connection URL
+            'username': 'your_username',
+            'password': 'your_password',
+            'authSource': 'admin',
+        }
+    }
+}
